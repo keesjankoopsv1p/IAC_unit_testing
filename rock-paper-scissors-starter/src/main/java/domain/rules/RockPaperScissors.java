55cc9c618e0e4332c39e4d279a643a2b779b95d9
keesjankoopsv1p <43411235+keesjankoopsv1p@users.noreply.github.com>
2020-06-24 01:40:57 +0200

    Add files via upload

---
+++ rock-paper-scissors-starter/src/main/java/domain/rules/RockPaperScissors.java
@@ -0,0 +1,40 @@
+package domain.rules;
+
+import domain.Move;
+import domain.Result;
+import domain.Rules;
+
+public class RockPaperScissors implements Rules {
+    @Override
+    public Result decide(Move mine, Move theirs) {
+        if (mine == theirs) {
+            return Result.DRAW;
+        }
+        else {
+            switch (mine) {
+                case ROCK -> {
+                    switch (theirs) {
+                        case SCISSORS -> {
+                            return Result.WIN;
+                        }
+                    }
+                }
+                case PAPER -> {
+                    switch (theirs) {
+                        case ROCK -> {
+                            return Result.WIN;
+                        }
+                    }
+                }
+                case SCISSORS -> {
+                    switch (theirs) {
+                        case PAPER -> {
+                            return Result.WIN;
+                        }
+                    }
+                }
+            }
+            return Result.LOSE;
+        }
+    }
+}
